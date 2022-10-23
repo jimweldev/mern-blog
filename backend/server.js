7c41f43accaafe803d90eff312a7ef548b6092ff
Jimwel Dizon <jimweldizon.dev@gmail.com>
2022-10-23 15:28:54 +0800

    profile stable

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,6 @@ app.use(cors())
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 
-app.use('/public', express.static('backend/public'))
-
 app.use((req, res, next) => {
    console.log(req.path, req.method)
    next()
